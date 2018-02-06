d2dad75ba7cb023a1da7a4b0b95a8b9237b5bde4
SimonBussy <simon.bussy@gmail.com>
2018-02-06 01:44:30 +0100

    computing times simulations

---
+++ simulations/run_simu_times.py
@@ -0,0 +1,157 @@
+import sys
+
+sys.path.append('../')
+import pandas as pd
+import numpy as np
+from time import time
+from sklearn.externals.joblib import Parallel, delayed
+from tick.simulation import SimuCoxRegWithCutPoints
+from tick.preprocessing.features_binarizer import FeaturesBinarizer
+from tick.inference import CoxRegression
+from binacox import get_p_values_j
+
+
+def get_times1(n_simu, n_samples, n_features, n_cut_points):
+    print("  n_simu=%s" % n_simu)
+    seed = n_simu
+    simu = SimuCoxRegWithCutPoints(n_samples=n_samples, n_features=n_features,
+                                   seed=seed, verbose=False,
+                                   n_cut_points=n_cut_points,
+                                   shape=2, scale=.1, cov_corr=cov_corr,
+                                   sparsity=sparsity)
+    X, Y, delta, cut_points, beta_star, S = simu.simulate()
+
+    # BinaCox method
+    tic = time()
+    n_cuts = 50
+    binarizer = FeaturesBinarizer(n_cuts=n_cuts)
+    X_bin = binarizer.fit_transform(X)
+    blocks_start = binarizer.blocks_start
+    blocks_length = binarizer.blocks_length
+    boundaries = binarizer.boundaries['0']
+
+    solver = 'agd'
+    learner = CoxRegression(penalty='binarsity', tol=1e-5,
+                            solver=solver, verbose=False,
+                            max_iter=100, step=0.3,
+                            blocks_start=blocks_start,
+                            blocks_length=blocks_length,
+                            C=25, warm_start=True)
+    learner._solver_obj.linesearch = False
+    learner.fit(X_bin, Y, delta)
+    tac = time()
+    time_bina = tac - tic
+
+    # Auto Cutoff Method
+    X = np.array(X)
+    epsilon = 10
+    p1 = np.percentile(X, epsilon)
+    p2 = np.percentile(X, 100 - epsilon)
+    values_to_test = X[np.where((X <= p2) & (X >= p1))]
+    tic = time()
+    get_p_values_j(X, 0, Y, delta, values_to_test, epsilon)
+    tac = time()
+    time_ac_all = tac - tic
+
+    tic = time()
+    p1 = np.percentile(X, epsilon)
+    p2 = np.percentile(X, 100 - epsilon)
+    values_to_test = boundaries[
+        np.where((boundaries <= p2) & (boundaries >= p1))]
+    get_p_values_j(X, 0, Y, delta, values_to_test, epsilon)
+    tac = time()
+    time_ac_grid = tac - tic
+
+    return n_samples, time_bina, time_ac_all, time_ac_grid
+
+
+def get_times2(n_simu, n_samples, n_features, n_cut_points):
+    print("  n_simu=%s" % n_simu)
+    seed = n_simu
+    simu = SimuCoxRegWithCutPoints(n_samples=n_samples, n_features=n_features,
+                                   seed=seed, verbose=False,
+                                   n_cut_points=n_cut_points,
+                                   shape=2, scale=.1, cov_corr=cov_corr,
+                                   sparsity=sparsity)
+    X, Y, delta, cut_points, beta_star, S = simu.simulate()
+
+    # BinaCox method
+    tic = time()
+    n_cuts = 50
+    binarizer = FeaturesBinarizer(n_cuts=n_cuts)
+    X_bin = binarizer.fit_transform(X)
+    blocks_start = binarizer.blocks_start
+    blocks_length = binarizer.blocks_length
+    solver = 'agd'
+    learner = CoxRegression(penalty='binarsity', tol=1e-5,
+                            solver=solver, verbose=False,
+                            max_iter=100, step=0.3,
+                            blocks_start=blocks_start,
+                            blocks_length=blocks_length,
+                            C=25, warm_start=True)
+    learner._solver_obj.linesearch = False
+    learner.fit(X_bin, Y, delta)
+    tac = time()
+
+    return tac - tic
+
+
+# first setting
+print("\nBinaCox vs. Auto Cutoff computing times")
+n_features = 1
+n_cut_points = 2
+cov_corr = .5
+sparsity = .2
+N_simu = 50
+n_samples_grid = [200, 300, 500, 1000, 2000]
+
+result_ = pd.DataFrame(columns=["n_samples", "time_bina", "time_ac_all",
+                                "time_ac_grid"])
+for i, n_samples in enumerate(n_samples_grid):
+    print("n_samples: %d/%d " % ((i + 1), len(n_samples_grid)))
+    result_n = Parallel(n_jobs=10)(
+        delayed(get_times1)(n_simu, n_samples, n_features,
+                            n_cut_points)
+        for n_simu in range(N_simu))
+    result_n = pd.DataFrame(result_n,
+                            columns=["n_samples", "time_bina", "time_ac_all",
+                                     "time_ac_grid"])
+    result_ = result_.append(result_n, ignore_index=True)
+
+result = pd.DataFrame(columns=["n", "method", "time"])
+tmp = pd.DataFrame(columns=["n", "method", "time"])
+tmp.n = result_.n_samples
+tmp.method = "BinaCox"
+tmp.time = result_.time_bina
+result = result.append(tmp, ignore_index=True)
+
+tmp.n = result_.n_samples
+tmp.method = "AC all"
+tmp.time = result_.time_ac_all
+result = result.append(tmp, ignore_index=True)
+
+tmp.n = result_.n_samples
+tmp.method = "AC grid"
+tmp.time = result_.time_ac_grid
+result = result.append(tmp, ignore_index=True)
+
+result.to_json("./results_data/time1")
+
+# second setting
+print("\nBinaCox computing times = f(p)")
+n_samples = 2000
+N_features = 20
+n_features_max = 100
+n_features_grid = np.unique(np.geomspace(2, n_features_max,
+                                         N_features).astype(int))
+result = pd.DataFrame()
+for i, n_features in enumerate(n_features_grid):
+    print("n_features: %d/%d " % ((i + 1), N_features))
+
+    result_n = Parallel(n_jobs=10)(
+        delayed(get_times2)(n_simu, n_samples, n_features,
+                            n_cut_points)
+        for n_simu in range(N_simu))
+    result[n_features] = result_n
+
+result.to_json("./results_data/time2")
